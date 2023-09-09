c5d1599f50b385de7acff20143ab50c33d34c9fd
dotson-matthew <matthewadotson@icloud.com>
2023-09-08 22:35:59 -0500

    fixed placeholder

---
+++ src/screens/LoginFailure.js
@@ -0,0 +1,22 @@
+import * as React from "react";
+import {
+    Button,
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    TouchableOpacity,
+    Dimensions,
+    Image,
+  } from "react-native";
+
+
+  function LoginFailure ({navigation}) {
+    return (
+        <View>
+            <Text> There was a login LoginFailure</Text>
+        </View>
+    )
+  }
+
+  export default LoginFailure;
